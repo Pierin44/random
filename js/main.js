1ef2432671b2dbfb616aa532bcd0e971f620b924
Pierin44 <51125641+Pierin44@users.noreply.github.com>
2020-10-15 16:22:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $("#pick").click(function() {
   // Get a random name, the winner
   var winner = String(nameArray[Math.floor(Math.random()*nameArray.length)]);
     
-  if (winner === "Pierin"||"pierin"||"candrian"||"Candrian"||"Nils"||"nils"||"Egger"||"egger")) {
+  if (winner == "Pierin"||"pierin"||"candrian"||"Candrian"||"Nils"||"nils"||"Egger"||"egger")) {
       winner = "Stöcklin"; }
     
   
